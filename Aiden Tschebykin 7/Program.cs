3e65e3f2b952f3553871cd5838fe29411c958802
Aiden Tschebykin <tschas01@purdue.edu>
2026-04-22 16:28:24 -0400

    Add project files.

---
+++ Aiden Tschebykin 7/Program.cs
@@ -0,0 +1,17 @@
+namespace Aiden_Tschebykin_7
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Project7Form());
+        }
+    }
+}
---
+++ Aiden Tschebykin 7/Project7Form.cs
@@ -0,0 +1,10 @@
+namespace Aiden_Tschebykin_7
+{
+    public partial class Project7Form : Form
+    {
+        public Project7Form()
+        {
+            InitializeComponent();
+        }
+    }
+}
